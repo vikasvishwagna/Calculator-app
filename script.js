be3cac83a152ef9ebe76461732803cacf040cd0e
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-02-18 11:42:15 +0530

    feat:Finalized calculator app with working buttons and error handling

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', function(ele){
     if(ele.target.innerHTML === "="){
-      string = eval(string);
-      document.querySelector('input').value = string;
+      try{
+        string = eval(string);
+        document.querySelector('input').value = string;
+      }catch(error){
+        document.querySelector('input').value = 'wrong input';
+        string="";
+      }
+      
     }else if(ele.target.innerHTML == 'RESET'){
       string = "";
       document.querySelector('input').value = string;
-    }else{
+    }else if(ele.target.innerHTML == 'DEL'){
+      string = string.slice(0,-1);
+      document.querySelector('input').value = string;
+    }
+    else{
       console.log(ele.target.innerHTML);
       string = string+ele.target.innerHTML;
       document.querySelector('input').value = string;
